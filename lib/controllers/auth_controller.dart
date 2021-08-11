fd39af8c900fdeedaa070c2b81d75ebb3ceb2548
Akron254 <aaronrono42@gmail.com>
2021-08-11 17:51:01 +0300

    Auth controller

---
+++ lib/controllers/auth_controller.dart
@@ -0,0 +1,155 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:gas_monitoring/config/app_url.dart';
+import 'package:gas_monitoring/models/user_model.dart';
+import 'package:gas_monitoring/services/user_preference.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:http/http.dart';
+
+class AuthController extends GetxController {
+  Future<Map<String, dynamic>> login(String email, String password) async {
+    var result;
+    final Map<String, dynamic> loginData = {
+      'email': email,
+      'password': password
+    };
+    print("Response Initiated");
+    Response response = await post(
+      Uri.parse(AppUrl.login),
+      body: json.encode(loginData),
+      headers: {'Content-Type': 'application/json'},
+    );
+    print(email);
+    print(response);
+    print("Response Received");
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      //var userData = responseData['data'];
+      User authUser = User.fromJson(responseData);
+      UserPreferences().saveUser(authUser);
+      print(responseData);
+      result = {'status': true, 'message': 'Successful', 'user': authUser};
+    } else {
+      result = {
+        'status': false,
+        'message': json.decode(response.body)['error']
+      };
+    }
+    return result;
+  }
+
+  Future<Map<String, dynamic>> getToken(String email, String password) async {
+    var result;
+    final Map<String, dynamic> loginData = {
+      'email': email,
+      'password': password
+    };
+    print("Response Initiated");
+    Response response = await post(
+      Uri.parse(AppUrl.login),
+      body: json.encode(loginData),
+      headers: {'Content-Type': 'application/json'},
+    );
+    print(email);
+    print(response);
+    print("Response Received");
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      //var userData = responseData['data'];
+      User authUser = User.fromJson(responseData);
+      UserPreferences().saveUser(authUser);
+      print(responseData);
+      result = {'status': true, 'message': 'Successful', 'user': authUser};
+    } else {
+      result = {
+        'status': false,
+        'message': json.decode(response.body)['error']
+      };
+    }
+    return result;
+  }
+
+  Future<Map<String, dynamic>> register(
+      {String firstName,
+      String lastName,
+      String email,
+      String password,
+      String phoneNumber}) async {
+    final Map<String, dynamic> registrationData = {
+      "firstName": firstName,
+      "lastName": lastName,
+      "email": email,
+      "userName": "$firstName $lastName",
+      "password": password,
+      "confirmPassword": password,
+      "phoneNumber": phoneNumber,
+      "activateUser": true,
+      "autoConfirmEmail": true
+    };
+
+    return await post(Uri.parse(AppUrl.register),
+            body: json.encode(registrationData),
+            headers: {'Content-Type': 'application/json'})
+        .then(onValue)
+        .catchError(onError);
+  }
+
+  Future<FutureOr> onValue(Response response) async {
+    var result;
+    final Map<String, dynamic> responseData = json.decode(response.body);
+    if (response.statusCode == 200) {
+      var userData = responseData['data'];
+      User authUser = User.fromJson(responseData);
+      UserPreferences().saveUser(authUser);
+      result = {
+        'status': true,
+        'message': 'Successfully registered',
+        'data': authUser
+      };
+    } else {
+      result = {
+        'status': false,
+        'message': 'Registration failed',
+        'data': responseData
+      };
+    }
+    return result;
+  }
+
+  Future<Map<String, dynamic>> verifyPass(String email, String password) async {
+    var result;
+    final Map<String, dynamic> loginData = {
+      'email': email,
+      'password': password
+    };
+    Response response = await post(
+      Uri.parse(AppUrl.login),
+      body: json.encode(loginData),
+      headers: {'Content-Type': 'application/json'},
+    );
+    print(email);
+    print(response);
+    print("Response Received");
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      //var userData = responseData['data'];
+      User authUser = User.fromJson(responseData);
+      UserPreferences().saveUser(authUser);
+      print(responseData);
+      result = {'status': true, 'message': 'Successful', 'user': authUser};
+    } else {
+      result = {
+        'status': false,
+        'message': json.decode(response.body)['error']
+      };
+    }
+    return result;
+  }
+
+  static onError(error) {
+    print("the error is $error.detail");
+    return {'status': false, 'message': 'Unsuccessful Request', 'data': error};
+  }
+}
--- lib/controllers/weight_controller.dart
+++ lib/controllers/weight_controller.dart
@@ -5,13 +5,14 @@ class WeightController extends GetxController {
   final volume = 0.0.obs;
   final volumePercentage = 0.0.obs;
 
+  // TODO: Set volume calculation algorithm(From density and weight)
+
   setWeight(double w) {
     weight(w);
     setVolume(w);
   }
 
   setVolume(double v) {
-    // TODO: Add volume calculation
     volume(v);
     volumePercentage(v);
   }

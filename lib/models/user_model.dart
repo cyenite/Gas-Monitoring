29c6cbe1a3cc86aa482420143eade5926448abee
Akron254 <aaronrono42@gmail.com>
2021-08-11 17:51:24 +0300

    Defined user model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,28 @@
+class User {
+  String firstName;
+  String lastName;
+  String email;
+  String username;
+  String token;
+  String renewalToken;
+  String activeVehicles;
+
+  User(
+      {this.email,
+        this.firstName,
+        this.lastName,
+        this.token,
+        this.renewalToken,
+        this.activeVehicles});
+
+  factory User.fromJson(Map<String, dynamic> responseData) {
+
+    return User(
+      email: responseData['email'],
+      firstName: responseData['firstName'],
+      lastName: responseData['lastName'],
+      token: responseData['token'],
+      renewalToken: responseData['renewal_token'],
+    );
+  }
+}
